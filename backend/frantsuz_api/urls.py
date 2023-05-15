e99165e6989a6d6eaf13028ad3f78cc61b95f4ed
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2023-05-15 13:09:24 +0300

    fix images

--- backend/frantsuz_api/urls.py
+++ backend/frantsuz_api/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -30,3 +32,8 @@ urlpatterns += [
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
