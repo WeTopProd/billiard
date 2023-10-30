3d6e7b040aaf7314a99ff97884a4867908500df3
94R1K <dfdfdf.dfdf.1985@mail.ru>
2023-10-30 16:07:19 +0300

    deploy

--- backend/goods/filters.py
+++ backend/goods/filters.py
@@ -59,7 +59,6 @@ class GoodsFilter(FilterSet):
         choices=Goods.AFFILIATION_GOODS,
         field_name='affiliation_goods'
     )
-
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart'
